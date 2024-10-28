82e6a98364e8251d2725f91e0f470a8a5f9d11f2
Romulo Rocha <romulorj2023@gmail.com>
2024-10-28 11:31:59 -0300

    hey-11: handling n+1 issue

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -9,7 +9,7 @@ class DashboardController extends Controller
 {
     public function __invoke(): View
     {
-        $questions = Question::all();
+        $questions = Question::with('votes')->get();
 
         return view('dashboard', compact('questions'));
     }
