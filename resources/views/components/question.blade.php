c55f7334354685c4be168792adb81d3ee96ef59b
Romulo Rocha <romulorj2023@gmail.com>
2024-10-29 20:45:00 -0300

    hey-18: it should order by like and unlinke

--- resources/views/components/question.blade.php
+++ resources/views/components/question.blade.php
@@ -7,14 +7,14 @@
         <x-form :action="route('question.like', $question)">
             <button class='flex items-center space-x-1 text-green-500'>
                 <x-icons.thumbs-up class='w-5 h-5 hover:text-green-300 cursor-pointer' />
-                <span>{{ $question->likes }}</span>
+                <span>{{ $question->votes_sum_like ?: 0 }}</span>
             </button>
         </x-form>
 
         <x-form :action="route('question.unlike', $question)">
             <button class='flex items-center space-x-1 text-red-500'>
                 <x-icons.thumbs-down class='w-5 h-5 hover:text-red-300 cursor-pointer' />
-                <span>{{ $question->unlikes }}</span>
+                <span>{{ $question->votes_sum_unlike ?: 0 }}</span>
             </button>
         </x-form>
 
