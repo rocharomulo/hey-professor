333fcb273974e26df3a1e0f5293be69f6d7c6e6e
Romulo Rocha <romulorj2023@gmail.com>
2024-10-29 11:31:42 -0300

    hey-16: make sure that only question with DRAFT status can be updated

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -47,6 +47,8 @@ class QuestionController extends Controller
 
     public function update(Question $question): RedirectResponse
     {
+        $this->authorize('update', $question);
+
         $attributes = request()->validate([
             'question' => [
                 'required',
--- tests/Feature/Question/UpdateTest.php
+++ tests/Feature/Question/UpdateTest.php
@@ -20,3 +20,30 @@ it('shoud be able to update a question', function () {
 
     expect($question->question)->toBe('updated question?');
 });
+
+test('make sure that only question with DRAFT status can be updated', function () {
+    //Arrange (preparar),
+    $user = User::factory()->create();
+    $this->actingAs($user);
+
+    $questionDraft    = Question::factory()->for($user, 'createdBy')->create(['draft' => true]);
+    $questionNotDraft = Question::factory()->for($user, 'createdBy')->create(['draft' => false]);
+
+    put(route('question.update', $questionNotDraft))->assertForbidden();
+    put(route('question.update', $questionDraft), ['question' => 'updated question?'])->assertRedirect();
+});
+
+it('should make sure that only the person who has created the question can update', function () {
+    //Arrange
+    $rightUser = User::factory()->create();
+    $wrongUser = User::factory()->create();
+
+    $question = Question::factory()->for($rightUser, 'createdBy')->create(['draft' => true]);
+
+    //Act (Agir)
+    $this->actingAs($wrongUser);
+    put(route('question.update', $question))->assertForbidden();
+
+    $this->actingAs($rightUser);
+    put(route('question.update', $question))->assertRedirect();
+});
